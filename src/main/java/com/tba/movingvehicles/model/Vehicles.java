06c581283741691a6a3249a897d13dce87e5a1b8
adnanrimedzo <adnanrimedzo@hotmail.com>
2017-07-18 00:24:52 +0300

    activeMQ added

--- src/main/java/com/tba/movingvehicles/model/Vehicles.java
+++ src/main/java/com/tba/movingvehicles/model/Vehicles.java
@@ -11,6 +11,7 @@ public class Vehicles extends JPanel {
     public Vehicles(List<Vehicle> vehicleList, ExecutorService executor) {
         setLayout(null);
         vehicleList.forEach(v -> {
+            Future future = executor.submit(v.getVehicleCallable());
             add(v);
         });
     }
